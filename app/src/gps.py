b6b35f0d055c01f2ddbeac529624685648628b93
william <william.flynt@gmail.com>
2018-07-27 09:56:22 -0700

    simplified comparisons for spd, trk

--- app/src/gps.py
+++ app/src/gps.py
@@ -1,12 +1,12 @@
 import datetime
 import logging
 import os
-import serial
-from cmath import rect, phase
 from collections import deque
-from math import radians, degrees
+from functools import partial
 
+import numpy as np
 import pynmea2
+import serial
 from pyubx import Manager
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -36,8 +36,8 @@ class CoordinateService(Manager):
                 ser = serial.Serial(ser)
         except Exception:
             logger.exception("********************\n"
-                             "      **GPS device error**\n"
-                             "      ********************")
+                             "**GPS device error**\n"
+                             "********************")
             raise
 
         logger.info("Setting up PyUBX Manager...")
@@ -58,14 +58,17 @@ class CoordinateService(Manager):
         self.s_i_max = s_i_max  # speed-instant trigger
         self.s_a_max = s_a_max  # speed-average trigger
         self.t_i_max = t_i_max  # track-instant trigger
-        self.t_a_max = t_a_max  # track-average trigger
+        # We compare sine values - so compute this now
+        # It's the max difference in sine value vs angular value
+        self.t_a_max = 7  # init greater than sine is possible
+        self.set_tamax(t_a_max)  # track-average trigger
 
         logger.info("GPS service initialized.")
 
     @staticmethod
     def hdg_diff(init, final):
         if init > 360 or init < 0 or final > 360 or final < 0:
-            raise Exception("out of range")
+            raise Exception("out of range. init: {} // final: {}".format(init, final))
         diff = final - init
         abs_diff = abs(diff)
 
@@ -79,8 +82,51 @@ class CoordinateService(Manager):
             return 360 - abs_diff
 
     @staticmethod
-    def mean_angle(deg):
-        return degrees(phase(sum(rect(1, radians(d)) for d in deg) / len(deg)))
+    def _vel_proc(vel_dict, v_or_k, index, funcname):
+        """
+        :param v_or_k: str Either 'keys' or 'values' - what do we want?
+        :param index: int The index of the key/value we want (typically 0 or 1)
+        :param vel_dict: dict The velocity dict: { timestamp: ( speed, track ) }
+        :return: float The requested value
+        """
+        # Check errors / log
+        if not isinstance(vel_dict, dict):
+            logger.error("*****{} was passed a non-dict parameter") \
+                .format(funcname)
+            return -1
+        if v_or_k not in ["values", "keys"]:
+            logger.error("*****{} was passed {}, not 'keys' or 'values'") \
+                .format(funcname, v_or_k)
+            return -1
+        # Do actual work
+        try:
+            return list(getattr(vel_dict, v_or_k)())[0][index]
+        except KeyError:
+            logger.error("*****{} encountered KeyError for {}" \
+                         .format(funcname, vel_dict))
+            return -1
+
+    _spd = partial(_vel_proc, v_or_k="values", index=0, funcname="_spd")
+    _trk = partial(_vel_proc, v_or_k="values", index=1, funcname="_trk")
+    _ts = partial(_vel_proc, v_or_k="keys", index=0, funcname="_ts")
+
+    @staticmethod
+    def avg_sin(angles):
+        """
+        Given a list of angles in degrees, return the average of the
+          individual sine values.
+        :param angles: list A list of angles in degrees
+        :return: float Average sine value for the list
+        """
+        avg = sum(np.sin(np.radians(angles))) / len(angles)
+        return round(avg, 4)
+
+    def set_tamax(self, new_max):  # Alternative is self.t_a_max as property
+        try:
+            self.t_a_max = np.sin(np.radians(new_max))
+        except Exception:
+            logger.error("*****Error computing new t_a_max for value {}"
+                         .format(new_max))
 
     def _check_velocity(self):
         try:
@@ -88,46 +134,54 @@ class CoordinateService(Manager):
         except IndexError:
             return
 
+        s_val = 0.0  # init outside func for use in comparison
+
         # Check speed for instant diff and average diff
         def speed_alarm():
-            s_val = self.vel_array[0][now][0]  # { now: (speed, track) }
+            s_val = self._spd(self.vel_array[0])  # { now: (speed, track) }
             """
             This gets the least-recent speed adhering to the vel_inst_seconds.
             The idea is to check less recently than the fastest rate of msgs.
             It will be the same for track (below).
             """
-            s_i_check_val = [x[0] for x in self.vel_array if
-                             list(x.keys())[0] >= (now - self.vel_inst_seconds)][-1]
-            s_a_check_list = [x[0] for x in self.vel_array if
-                              list(x.keys())[0] >= (now - self.vel_avg_seconds)]
+            s_i_check_val = [self._spd(x) for x in self.vel_array if
+                             self._ts(x) >= (now - self.vel_inst_seconds)][-1]
+            s_a_check_list = [self._spd(x) for x in self.vel_array if
+                              self._ts(x) >= (now - self.vel_avg_seconds)]
             s_a_check_val = sum(s_a_check_list) / len(s_a_check_list)
-            return (abs(s_val - s_i_check_val) > self.s_i_max or
-                    abs(s_val - s_a_check_val) > self.s_a_max)
+            alarm = (abs(s_val - s_i_check_val) > self.s_i_max or
+                     abs(s_val - s_a_check_val) > self.s_a_max)
+            return alarm
 
         # Check speed for instant diff and average diff
         def track_alarm():
-            t_val = abs(self.vel_array[0][now][1])  # { now: (speed, track) }
-            t_i_check_val = [x[1] for x in self.vel_array if
-                             list(x.keys())[0] >= (now - self.vel_inst_seconds)][-1]
+            t_val = self._trk(self.vel_array[0])  # { now: (speed, track) }
+            t_i_check_val = [self._trk(x) for x in self.vel_array if
+                             self._ts(x) >= (now - self.vel_inst_seconds)][-1]
             # Compute average heading over time
-            t_a_check_val = self.mean_angle(
-                [x[1] for x in self.vel_array if
-                 list(x.keys())[0] >= (now - self.vel_avg_seconds)]
-            )
-            return (self.hdg_diff(t_val, t_i_check_val) > self.t_i_max or
-                    self.hdg_diff(t_val, t_a_check_val) > self.t_a_max)
+            t_a_check_val = self.avg_sin([self._trk(x) for x in self.vel_array if
+                                          self._ts(x) >= (now - self.vel_avg_seconds)])
+            alarm = (self.hdg_diff(t_val, t_i_check_val) > self.t_i_max or
+                     abs(t_val - t_a_check_val) > self.t_a_max)
+            return alarm
 
         try:
-            if speed_alarm() or track_alarm():
+            if speed_alarm() or (s_val > 2.2 and track_alarm()):  # speed at rest 0-2.2
                 try:
                     self.parent.msg_alarm = 1
                 except AttributeError:
                     if self.parent:
                         logger.warning("*****Error accessing parent.msg_alarm.", exc_info=True)
+                except IndexError:
+                    logger.warning("*****IndexError trying to get velocity alarms")
+        except KeyError:
+            logger.exception("********************\n"
+                             "***velocity error***\n"
+                             "********************")
         except Exception:
             logger.exception("********************\n"
-                             "           ***velocity error***\n"
-                             "           ********************")
+                             "***velocity error***\n"
+                             "********************")
 
     # Override onNMEA from parent class to do work
     def onNMEA(self, buffer):
@@ -139,6 +193,16 @@ class CoordinateService(Manager):
             try:
                 speed = msg.spd_over_grnd_kmph
                 track = msg.true_track
+                # TODO REMOVE
+                from random import randint, random
+                if speed > 100:
+                    speed = speed * .1
+                elif speed < 10:
+                    speed = speed * randint(1, 20) / 10.0
+                elif 10 >= speed <= 100:
+                    speed = speed * randint(85, 115) / 100.0
+                if not track:
+                    track = randint(900, 2700) / 10.0
                 if speed and track:
                     # now = timestamp in seconds
                     now = datetime.datetime.timestamp(datetime.datetime.now())
@@ -172,5 +236,5 @@ class CoordinateService(Manager):
             return 0
         except Exception:
             logger.exception("********************\n"
-                             "           **latest_vel error**\n"
-                             "           ********************")
+                             "**latest_vel error**\n"
+                             "********************")
