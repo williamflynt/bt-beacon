2eca21b53dc91ad41b0355429d7dc2247e0dd8e5
william <william.flynt@gmail.com>
2019-02-01 13:25:28 -0800

    add commented settings for in a house vs RAIN

--- app/src/locate.py
+++ app/src/locate.py
@@ -44,8 +44,13 @@ class BeaconLocator(SubscribeCallback):
         # beacon_matrix = {
         #     "bluecharm": -59.8,
         # }
-        self.n = 2.5  # Path loss exponent = 1.6-1.8 w/LOS to beacon indoors
-        self.measured_rssi = -50.5  # Beacon-specific measured RSSI @ 1m
+        # Settings for inside RAIN
+        self.n = 3.1  # Path loss exponent = 1.6-1.8 w/LOS to beacon indoors
+        self.measured_rssi = -46  # Beacon-specific measured RSSI @ 1m
+
+        # Settings for inside a normal house
+        # self.n = 2.7
+        # self.measured_rssi = -59.8
 
         self.solver = TrilaterationSolver()
 
